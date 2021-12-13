ce902f894fe1a59c9a7f4cdf73c8acc0eccb87cd
SimplyNate <horiuchi.nathan@gmail.com>
2021-12-12 19:15:19 -1000

    Using basic grid to group similar attributes together, do telemetry conversion onmessage

--- src/components/ForzaTelemetry.vue
+++ src/components/ForzaTelemetry.vue
@@ -1,44 +1,51 @@
 <template>
-    <div>
-        <div>currentEngineRpm: {{ telemetry.currentEngineRpm }}</div>
-        <div>normSuspensionTravelFl: {{ telemetry.normSuspensionTravelFl }}</div>
-        <div>normSuspensionTravelFr: {{ telemetry.normSuspensionTravelFr }}</div>
-        <div>normSuspensionTravelRl: {{ telemetry.normSuspensionTravelRl }}</div>
-        <div>normSuspensionTravelRr: {{ telemetry.normSuspensionTravelRr }}</div>
-        <div>tireSlipRatioFl: {{ telemetry.tireSlipRatioFl }}</div>
-        <div>tireSlipRatioFr: {{ telemetry.tireSlipRatioFr }}</div>
-        <div>tireSlipRatioRl: {{ telemetry.tireSlipRatioRl }}</div>
-        <div>tireSlipRatioRr: {{ telemetry.tireSlipRatioRr }}</div>
-        <div>wheelRotationSpeedFl: {{ telemetry.wheelRotationSpeedFl }}</div>
-        <div>wheelRotationSpeedFr: {{ telemetry.wheelRotationSpeedFr }}</div>
-        <div>wheelRotationSpeedRl: {{ telemetry.wheelRotationSpeedRl }}</div>
-        <div>wheelRotationSpeedRr: {{ telemetry.wheelRotationSpeedRr }}</div>
-        <div>tireSlipAngleFl: {{ telemetry.tireSlipAngleFl }}</div>
-        <div>tireSlipAngleFr: {{ telemetry.tireSlipAngleFr }}</div>
-        <div>tireSlipAngleRl: {{ telemetry.tireSlipAngleRl }}</div>
-        <div>tireSlipAngleRr: {{ telemetry.tireSlipAngleRr }}</div>
-        <div>tireCombinedSlipFl: {{ telemetry.tireCombinedSlipFl }}</div>
-        <div>tireCombinedSlipFr: {{ telemetry.tireCombinedSlipFr }}</div>
-        <div>tireCombinedSlipRl: {{ telemetry.tireCombinedSlipRl }}</div>
-        <div>tireCombinedSlipRr: {{ telemetry.tireCombinedSlipRr }}</div>
-        <div>suspensionTravelMetersFl: {{ telemetry.suspensionTravelMetersFl }}</div>
-        <div>suspensionTravelMetersFr: {{ telemetry.suspensionTravelMetersFr }}</div>
-        <div>suspensionTravelMetersRl: {{ telemetry.suspensionTravelMetersRl }}</div>
-        <div>suspensionTravelMetersRr: {{ telemetry.suspensionTravelMetersRr }}</div>
-        <div>speed: {{ telemetry.speed * conversions.speed }}</div>
-        <div>power: {{ telemetry.power / conversions.power }}</div>
-        <div>torque: {{ telemetry.torque }}</div>
-        <div>tireTempFl: {{ telemetry.tireTempFl }}</div>
-        <div>tireTempFr: {{ telemetry.tireTempFr }}</div>
-        <div>tireTempRl: {{ telemetry.tireTempRl }}</div>
-        <div>tireTempRr: {{ telemetry.tireTempRr }}</div>
-        <div>boost: {{ telemetry.boost }}</div>
-        <div>accelerator: {{ telemetry.accelerator }}</div>
-        <div>brake: {{ telemetry.brake }}</div>
-        <div>clutch: {{ telemetry.clutch }}</div>
-        <div>handbrake: {{ telemetry.handbrake }}</div>
-        <div>gear: {{ telemetry.gear }}</div>
-        <div>steer: {{ telemetry.steer }}</div>
+    <div class="grid-container">
+        <div class="grid-item controls">
+            <div>accelerator: {{ telemetry.accelerator }}</div>
+            <div>brake: {{ telemetry.brake }}</div>
+            <div>clutch: {{ telemetry.clutch }}</div>
+            <div>handbrake: {{ telemetry.handbrake }}</div>
+            <div>steer: {{ telemetry.steer }}</div>
+        </div>
+        <div class="grid-item tires">
+            <div>normSuspensionTravelFl: {{ telemetry.normSuspensionTravelFl }}</div>
+            <div>normSuspensionTravelFr: {{ telemetry.normSuspensionTravelFr }}</div>
+            <div>normSuspensionTravelRl: {{ telemetry.normSuspensionTravelRl }}</div>
+            <div>normSuspensionTravelRr: {{ telemetry.normSuspensionTravelRr }}</div>
+            <div>suspensionTravelMetersFl: {{ telemetry.suspensionTravelMetersFl }}</div>
+            <div>suspensionTravelMetersFr: {{ telemetry.suspensionTravelMetersFr }}</div>
+            <div>suspensionTravelMetersRl: {{ telemetry.suspensionTravelMetersRl }}</div>
+            <div>suspensionTravelMetersRr: {{ telemetry.suspensionTravelMetersRr }}</div>
+
+            <div>tireSlipRatioFl: {{ telemetry.tireSlipRatioFl }}</div>
+            <div>tireSlipRatioFr: {{ telemetry.tireSlipRatioFr }}</div>
+            <div>tireSlipRatioRl: {{ telemetry.tireSlipRatioRl }}</div>
+            <div>tireSlipRatioRr: {{ telemetry.tireSlipRatioRr }}</div>
+            <div>wheelRotationSpeedFl: {{ telemetry.wheelRotationSpeedFl }}</div>
+            <div>wheelRotationSpeedFr: {{ telemetry.wheelRotationSpeedFr }}</div>
+            <div>wheelRotationSpeedRl: {{ telemetry.wheelRotationSpeedRl }}</div>
+            <div>wheelRotationSpeedRr: {{ telemetry.wheelRotationSpeedRr }}</div>
+            <div>tireSlipAngleFl: {{ telemetry.tireSlipAngleFl }}</div>
+            <div>tireSlipAngleFr: {{ telemetry.tireSlipAngleFr }}</div>
+            <div>tireSlipAngleRl: {{ telemetry.tireSlipAngleRl }}</div>
+            <div>tireSlipAngleRr: {{ telemetry.tireSlipAngleRr }}</div>
+            <div>tireCombinedSlipFl: {{ telemetry.tireCombinedSlipFl }}</div>
+            <div>tireCombinedSlipFr: {{ telemetry.tireCombinedSlipFr }}</div>
+            <div>tireCombinedSlipRl: {{ telemetry.tireCombinedSlipRl }}</div>
+            <div>tireCombinedSlipRr: {{ telemetry.tireCombinedSlipRr }}</div>
+            <div>tireTempFl: {{ telemetry.tireTempFl }}</div>
+            <div>tireTempFr: {{ telemetry.tireTempFr }}</div>
+            <div>tireTempRl: {{ telemetry.tireTempRl }}</div>
+            <div>tireTempRr: {{ telemetry.tireTempRr }}</div>
+        </div>
+        <div class="grid-item power">
+            <div>currentEngineRpm: {{ telemetry.currentEngineRpm }}</div>
+            <div>gear: {{ telemetry.gear }}</div>
+            <div>speed: {{ telemetry.speed }}</div>
+            <div>power: {{ telemetry.power }}</div>
+            <div>torque: {{ telemetry.torque }}</div>
+            <div>boost: {{ telemetry.boost }}</div>
+        </div>
     </div>
 </template>
 
@@ -69,15 +76,19 @@ export default defineComponent({
         const ws = new WebSocket('ws://localhost:3001');
         ws.onmessage = (payload) => {
             this.telemetry = JSON.parse(payload.data);
-            if (this.telemetry) {
-                this.telemetry.speed *= this.conversions.speed;
-                this.telemetry.power /= this.conversions.power;
-            }
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            this.telemetry.speed *= this.conversions.speed;
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            this.telemetry.power /= this.conversions.power;
         }
     },
 });
 </script>
 
 <style scoped lang="scss">
-
+.grid-container {
+    display: grid;
+}
 </style>
