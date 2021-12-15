6d0e22dc3d1866399ed6e0a0f7449b0a14df67d7
SimplyNate <horiuchi.nathan@gmail.com>
2021-12-14 18:22:33 -1000

    Added stubs for Tire & Suspension visualizations and a vertical progress bar

---
+++ src/components/TireSuspension.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'TireSuspension',
+    props: {
+        side: String,
+        values: Object,
+    }
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/VerticalProgress.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'VerticalProgress',
+
+});
+</script>
+
+<style scoped>
+
+</style>
